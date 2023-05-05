3332fb3b005e4f4df389c93b714e7f1adbe44329
Kisakay <53227802+Kisakay@users.noreply.github.com>
2023-05-05 19:15:11 +0200

    Added message when iHorizon join a guild

--- files/Events/guildDelete.js
+++ files/Events/guildDelete.js
@@ -2,10 +2,31 @@ const { Client, Intents, Collection, EmbedBuilder, Permissions } = require('disc
 const fs = require("fs")
 const { QuickDB } = require("quick.db");
 const db = new QuickDB();
+const config = require(`${process.cwd()}/files/config.json`)
 
-module.exports = async (client, guild) => {    
-    client.invites.delete(guild.id);
+module.exports = async (client, guild) => {
 
-    await db.delete(`${guild.id}`);
-    await db.delete(`${guild.id}`);
+    async function inviteManager() {
+        await db.delete(`${guild.id}`);
+        await db.delete(`${guild.id}`);
+
+        return client.invites.delete(guild.id);
+    }
+    async function ownerLogs() {
+        let embed = new EmbedBuilder()
+            .setColor("#ff0505")
+            .setTimestamp(guild.joinedTimestamp)
+            .setDescription(`**A guild have deleted iHorizon !**`)
+            .addFields({ name: "🏷️・Server Name", value: `\`${guild.name}\``, inline: true },
+                { name: "🆔・Server ID", value: `\`${guild.id}\``, inline: true },
+                { name: "🌐・Server Region", value: `\`${guild.preferredLocale}\``, inline: true },
+                { name: "👤・MemberCount", value: `\`${guild.memberCount}\` members`, inline: true },
+                { name: "🪝・Vanity URL", value: `\`discord.gg/${guild.vanityURLCode || "None"}\``, inline: true })
+            .setThumbnail(`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`)
+            .setFooter({ text: 'iHorizon', iconURL: client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 }) })
+
+        return client.channels.cache.get(config.guildLogsChannelID).send({ embeds: [embed] }).catch(() => { });
+    }
+
+    await inviteManager(), ownerLogs();
 }
