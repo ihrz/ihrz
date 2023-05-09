7931ac4efbd110306f01c20739b1fc62570a506d
Kisa Keyara <53227802+Kisakay@users.noreply.github.com>
2023-05-09 01:40:10 +0100

    Fixe some useless problem

--- files/Slash/bot/kisakay.js
+++ files/Slash/bot/kisakay.js
@@ -1,13 +1,13 @@
 const yaml = require('js-yaml'),
-fs = require('fs');
+        fs = require('fs');
 
 module.exports = {
         name: 'kisakay',
         description: 'What is that?',
         run: async (client, interaction) => {
-                let fileContents = fs.readFileSync(process.cwd()+"/files/lang/en-US.yml", 'utf-8'); //
-                let data = yaml.load(fileContents)
-
+                let fileContents = fs.readFileSync(process.cwd() + "/files/lang/en-US.yml", 'utf-8');
+                let data = yaml.load(fileContents);
+                
                 return interaction.reply(data.kisakay_message);
         }
 }
