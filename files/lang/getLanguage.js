f490deb945bae533c2b45ddf41b16b0595dbb4b6
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-05-14 22:16:16 -0400

    Update getLanguage.js
    Added a ;
    (Sorry..)

--- files/lang/getLanguage.js
+++ files/lang/getLanguage.js
@@ -2,7 +2,7 @@ const { QuickDB } = require("quick.db");
 const db = new QuickDB();
 
 async function getLanguage(arg) {
-    let fetched = await db.get(`${arg}.GUILD.LANG`)
+    let fetched = await db.get(`${arg}.GUILD.LANG`);
     if(!fetched) {
         return "en-US";
     }
