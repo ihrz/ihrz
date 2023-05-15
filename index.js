5cf2bc125c6f350bc45fc9c0c89f89afd84ba237
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-05-14 22:56:48 -0400

    Update index.js
    Even shorter than before.
    6 lines instead of 9 lines.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/*Made by Kisakay*/
 const { Client } = require('discord.js');
 const clientIntents = require('./files/core/clientIntents.js');
 const clientPartial = require('./files/core/clientPartial.js');
-
 const client = new Client({ intents: clientIntents, partials: clientPartial, ws: { properties: { browser: 'Discord iOS' } } });
 const core = require('./files/core')(client);
-
-client.login(require('./files/config.json').token)
+client.login(require('./files/config.json').token);
