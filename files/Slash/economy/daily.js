9a7e6765e0a36666af657d44cdeef44c21b53386
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-05-22 08:13:18 -0400

    Update daily.js
    Grammar

--- files/Slash/economy/daily.js
+++ files/Slash/economy/daily.js
@@ -1,6 +1,6 @@
 const { QuickDB } = require("quick.db");
 const db = new QuickDB();
-const ms = require('ms')
+const ms = require('ms');
 const {
   Client,
   Intents,
@@ -18,19 +18,18 @@ const getLanguage = require(`${process.cwd()}/files/lang/getLanguage`);
 
 module.exports = {
   name: 'daily',
-  description: 'Earn your daily gain from you work',
+  description: 'Claim your daily gain',
   run: async (client, interaction) => {
     let fileContents = fs.readFileSync(`${process.cwd()}/files/lang/${await getLanguage(interaction.guild.id)}.yml`, 'utf-8');
     let data = yaml.load(fileContents);
 
-    let timeout = 86400000
-    let amount = 500
+    let timeout = 86400000;
+    let amount = 500;
     let daily = await db.get(`${interaction.guild.id}.USER.${interaction.user.id}.ECONOMY.daily`);
-
     if (daily !== null && timeout - (Date.now() - daily) > 0) {
       let time = ms(timeout - (Date.now() - daily));
 
-      interaction.reply({ content: data.daily_cooldown_error.replace(/\${time}/g, time) })
+      interaction.reply({ content: data.daily_cooldown_error.replace(/\${time}/g, time) });
     } else {
       let embed = new EmbedBuilder()
         .setAuthor({ name: data.daily_embed_title, iconURL: `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.png` })
@@ -38,10 +37,9 @@ module.exports = {
         .setDescription(data.daily_embed_description)
         .addFields({ name: data.daily_embed_fields, value: `${amount}🪙` })
 
-      interaction.reply({ embeds: [embed] })
-      db.add(`${interaction.guild.id}.USER.${interaction.user.id}.ECONOMY.money`, amount)
-      db.set(`${interaction.guild.id}.USER.${interaction.user.id}.ECONOMY.daily`, Date.now())
-
+      interaction.reply({ embeds: [embed] });
+      db.add(`${interaction.guild.id}.USER.${interaction.user.id}.ECONOMY.money`, amount);
+      db.set(`${interaction.guild.id}.USER.${interaction.user.id}.ECONOMY.daily`, Date.now());
     }
   }
 }
