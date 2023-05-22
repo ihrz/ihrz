28b3a1d90a250e22114cba6c391a1650f387807d
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-05-22 08:11:25 -0400

    Update balance.js
    Grammar boring things

--- files/Slash/economy/balance.js
+++ files/Slash/economy/balance.js
@@ -15,22 +15,22 @@ const getLanguage = require(`${process.cwd()}/files/lang/getLanguage`);
 const yaml = require('js-yaml'), fs = require('fs');
 module.exports = {
     name: 'balance',
-    description: 'show how much dollars you have in your bank',
+    description: 'shows how much money you have in your bank',
     options: [
         {
             name: 'user',
             type: ApplicationCommandOptionType.User,
-            description: 'Target a user for see their current balance bank or keep blank for yourself',
+            description: 'Target a user for see their current balance or keep blank for yourself',
             required: false
         }
     ],
     run: async (client, interaction) => {
         let fileContents = fs.readFileSync(`${process.cwd()}/files/lang/${await getLanguage(interaction.guild.id)}.yml`, 'utf-8');
-        let data = yaml.load(fileContents)
+        let data = yaml.load(fileContents);
         const member = interaction.options.get('user');
 
         if (!member) {
-            var bal = await db.get(`${interaction.guild.id}.USER.${interaction.user.id}.ECONOMY.money`)
+            var bal = await db.get(`${interaction.guild.id}.USER.${interaction.user.id}.ECONOMY.money`);
             if (!bal) {
                 return await db.set(`${interaction.guild.id}.USER.${interaction.user.id}.ECONOMY.money`, 1),
                     interaction.reply({ content: data.balance_dont_have_wallet })
@@ -38,10 +38,10 @@ module.exports = {
             interaction.reply({
                 content: data.balance_have_wallet
                     .replace(/\${bal}/g, bal)
-            })
+            });
         } else {
             if (member) {
-                var bal = await db.get(`${interaction.guild.id}.USER.${member.value}.ECONOMY.money`)
+                var bal = await db.get(`${interaction.guild.id}.USER.${member.value}.ECONOMY.money`);
                 if (!bal) {
                     return db.set(`${interaction.guild.id}.USER.${member.value}.ECONOMY.money`, 1),
                         interaction.reply({
@@ -51,7 +51,7 @@ module.exports = {
                 }
                 interaction.reply({
                     content: data.balance_he_have_wallet.replace(/\${bal}/g, bal)
-                })
+                });
             }
         }
     }
