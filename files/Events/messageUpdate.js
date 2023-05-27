20fbae1091448f0eae11698b17e6d72cef066f4a
Kisa Keyara <53227802+Kisakay@users.noreply.github.com>
2023-05-27 20:50:27 +0100

    Update messageUpdate.js

--- files/Events/messageUpdate.js
+++ files/Events/messageUpdate.js
@@ -17,15 +17,15 @@ module.exports = async (client, oldMessage, newMessage) => {
         const someinfo = await db.get(`${guildId}.GUILD.SERVER_LOGS.message`);
 
         if (!someinfo) return;
-
+        if(!oldMessage.content || !newMessage.content ) return;
         let logsEmbed = new EmbedBuilder()
             .setColor("#000000")
             .setAuthor({ name: newMessage.author.username, iconURL: newMessage.author.avatarURL({ format: 'png', dynamic: true, size: 512 }) })
             .setDescription(data.event_srvLogs_messageUpdate_description
                 .replace("${oldMessage.channelId}", oldMessage.channelId)
             )
-            .setFields({ name: data.event_srvLogs_messageUpdate_footer_1, value: oldMessage.content || "  "},
-                { name: data.event_srvLogs_messageUpdate_footer_2, value: newMessage.content || "  "})
+            .setFields({ name: data.event_srvLogs_messageUpdate_footer_1, value: oldMessage.content},
+                { name: data.event_srvLogs_messageUpdate_footer_2, value: newMessage.content})
             .setTimestamp();
 
         await client.channels.cache.get(someinfo).send({ embeds: [logsEmbed] }).catch(() => { });
