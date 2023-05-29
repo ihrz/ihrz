bbd70eeabfb313fac6805e8202243e8ab2daedf8
Kisa Keyara <53227802+Kisakay@users.noreply.github.com>
2023-05-29 10:05:05 +0100

    Update code.js

--- src/api/code/code.js
+++ src/api/code/code.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
     if (tor == "CHECK_IN_SYSTEM") {
         const { userid, adminKey } = req.body;
         if (!userid || !adminKey) return logger.warn("-> Bad json request without ip/key");
-        if (adminKey != require(`${process.cwd()}/src/config.js`).api.apiToken) return;
+        if (adminKey != require(`${process.cwd()}/files/config.js`).api.apiToken) return;
         let value = await db.get(`API.TOKEN.${userid}`); if (!value) { return res.json({ available: "no", id: userid, adminKey: "ok" }); };
         try { await oauth.getUser(value.token); res.json({ connectionToken: value.token, available: "yes", id: userid, adminKey: "ok" });
         } catch (e) { await db.delete(`API.TOKEN.${userid}`); return res.json({ available: "no", id: "deleted", adminKey: "ok" }); };
