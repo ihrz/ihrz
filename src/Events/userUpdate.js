bebe809a32286257a4bb54616f6a87ef9324f97b
Kisa Keyara <53227802+Kisakay@users.noreply.github.com>
2023-06-01 20:09:20 +0200

    Update userUpdate.js

--- src/Events/userUpdate.js
+++ src/Events/userUpdate.js
@@ -3,7 +3,13 @@ const { time } = require('discord.js');
 
 module.exports = async (client, oldUser) => {
     async function serverLogs() {
+        oldUsertag = oldUser.username+'#'+oldUser.discriminator;
+        newUser = await client.users.fetch(oldUser.id);
+        newUsertag = newUser.username+'#'+newUser.discriminator;
+
+        if (oldUsertag === newUsertag) return;
         if (!oldUser) return;
+        
         const someinfo = await db.get(`DB.PREVNAMES.${oldUser.id}`);
         var char = `${time((new Date()), 'd')} - ${oldUser.username}#${oldUser.discriminator}`;
         if (someinfo) { await db.push(`DB.PREVNAMES.${oldUser.id}`, char); } else { await db.push(`DB.PREVNAMES.${oldUser.id}`, char) }
