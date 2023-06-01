5f9212861619b719b7488779479513c77c82e6ec
Kisa Keyara <53227802+Kisakay@users.noreply.github.com>
2023-06-01 21:53:50 +0200

    More hacking 👩‍💻

--- src/Slash/fun/hack.js
+++ src/Slash/fun/hack.js
@@ -31,15 +31,33 @@ module.exports = {
             '100',
             '168',
             '254',
-            '345'
-
+            '345',
+            '128',
+            '256',
+            '255',
+            '0',
+            '144',
+            '38',
+            '67',
+            '97',
+            '32',
+            '64'
         ];
         var Email = [
             'discordmoderator@gmail.com',
             'fbi.open.up@gmail.com',
             'fan.ofdream@gmail.com',
             'bruhmoment@gmail.com',
-            'elhax0r1337@anonymail.ru'
+            'elhax0r1337@anonymail.ru',
+            'vladimir@gmail.ru',
+            'elonmusk@yahoo.com',
+            'emanuel.macron@gov.skem.ru',
+            'misterbeastcontact@gmail.com',
+            'misterbeastpro@gmail.com',
+            'fghjkgbnfvbjifvbjutgbnjiuhn@gmail.com',
+            'contact@ovh.cloud',
+            'jobs@discord.com',
+            'poutin@yandex.ru',
         ];
         var Password = [
             'sxmfg22',
@@ -47,9 +65,24 @@ module.exports = {
             'ursoxko332',
             'password223',
             'roblox556',
-            '123'
+            '123',
+            'password',
+            'minecraft595',
+            '0123456789',
+            'Password!',
+            '31012005'
         ];
 
+        // make a fonction for generate random number with lengt of 8 digits
+        function generateRandomNumber() {
+            var text = "";
+            var possible = "0123456789";
+            for (var i = 0; i < 8; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+            return text;
+        };
+
+        var PhoneNumber = ["+33", "+34", "+35", "+36", "+37", "+38", "+39", "+40", "+41", "+42", "+43", "+44", "+45", "+46", "+47", "+48", "+49", "+50", "+51", "+52", "+53", "+54", "+55"];
         const embed = new EmbedBuilder()
             .setColor("#800000")
             .setDescription(data.hack_embed_description
@@ -58,9 +91,10 @@ module.exports = {
                 )
             .addFields({ name: data.hack_embed_fields_ip, value: `\`${ip[Math.floor(Math.random() * ip.length)]}.${ip[Math.floor(Math.random() * ip.length)]}.${ip[Math.floor(Math.random() * ip.length)]}.${ip[Math.floor(Math.random() * ip.length)]}\`` },
                 { name: data.hack_embed_fields_email, value: `\`${Email[Math.floor(Math.random() * Email.length)]}\`` },
+                { name: "☎", value: `\`${PhoneNumber[Math.floor(Math.random() * PhoneNumber.length)]}${generateRandomNumber()}\`` },
                 { name: data.hack_embed_fields_password, value: `\`${Password[Math.floor(Math.random() * Password.length)]}\`` })
             .setTimestamp()
 
-        return interaction.reply({ embeds: [embed] });
+        return interaction.reply({ embeds: [embed] });/hack user: @Eto
     }
 };
