5506f76d68e8c4e5354e17e747f8a0727e43090c
Kisa Keyara <53227802+Kisakay@users.noreply.github.com>
2023-06-15 21:55:30 +0200

    Update transcript.js

--- src/Slash/ticket/transcript.js
+++ src/Slash/ticket/transcript.js
@@ -23,7 +23,7 @@ const slashInfo = require(`${process.cwd()}/files/ihorizon-api/slashHandler`);
 const DataBaseModel = require(`${process.cwd()}/files/ihorizon-api/main.js`);
 
 const sourcebin = require('sourcebin_js');
-const { Client, Intents, Collection, EmbedBuilder, Permissions } = require('discord.js');
+const { Client, Intents, Collection, EmbedBuilder, PermissionsBitField } = require('discord.js');
 
 slashInfo.ticket.transcript.run = async (client, interaction) => {
 	const getLanguageData = require(`${process.cwd()}/src/lang/getLanguageData`);
@@ -38,7 +38,7 @@ slashInfo.ticket.transcript.run = async (client, interaction) => {
 	const channel = interaction.channel;
 
 	if (channel.name.includes('ticket-')) {
-		if (!interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR) || channel.name === `ticket-${interaction.user.id}`) {
+		if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator) || channel.name === `ticket-${interaction.user.id}`) {
 			channel.messages.fetch().then(async (messages) => {
 				const output = messages.reverse().map(m => `${new Date(m.createdAt).toLocaleString('en-US')} - ${m.author.tag}: ${m.attachments.size > 0 ? m.attachments.first().proxyURL : m.content}`).join('\n');
 
