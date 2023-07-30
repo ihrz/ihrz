9b3e2258936226df09b6a64d95c8c058d3646988
Anaïs Saraiva <140051144+Mxi1n@users.noreply.github.com>
2023-07-30 17:47:20 +0200

    anaïs, get out
    Fixed the .setFields embed error

--- src/Events/messageUpdate.ts
+++ src/Events/messageUpdate.ts
@@ -47,8 +47,8 @@ export = async (client: Client, oldMessage: Message, newMessage: Message) => {
             .setDescription(data.event_srvLogs_messageUpdate_description
                 .replace("${oldMessage.channelId}", oldMessage.channelId)
             )
-            .setFields({ name: data.event_srvLogs_messageUpdate_footer_1, value: oldMessage.content || '** **' },
-                { name: data.event_srvLogs_messageUpdate_footer_2, value: newMessage.content || '** **' })
+            .setFields({ name: data.event_srvLogs_messageUpdate_footer_1, value: oldMessage.content || '**?**' },
+                { name: data.event_srvLogs_messageUpdate_footer_2, value: newMessage.content || '**?**' })
             .setTimestamp();
 
         await Msgchannel.send({ embeds: [logsEmbed] }).catch(() => { });
