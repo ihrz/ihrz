c4b60225fdbe3cdb6e0e6e55f38f5ac597e5ec89
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2023-09-15 19:37:40 +0200

    Update !mass-mention.ts

--- src/Commands/guildconfig/!mass-mention.ts
+++ src/Commands/guildconfig/!mass-mention.ts
@@ -57,64 +57,70 @@ export = {
             return;
 
         } else if (turn === "on") {
-            let arrayActionsForRule: Action[] = [
-                {
-                    type: 1,
-                    metadata: {
-                        customMessage: "This message was prevented by iHorizon"
-                    }
-                },
-            ];
 
-            if (logs_channel) {
-                arrayActionsForRule.push({
-                    type: 2,
-                    metadata: {
-                        channel: logs_channel,
-                    }
-                });
-            };
+            try {
 
-            if (!mentionSpamRule) {
-
-                await interaction.guild.autoModerationRules.create({
-                    name: 'Block mass-mention spam by iHorizon',
-                    creatorId: client.user?.id,
-                    enabled: true,
-                    eventType: 1,
-                    triggerType: 5,
-                    triggerMetadata:
+                let arrayActionsForRule: Action[] = [
                     {
-                        mentionTotalLimit: max_mention,
-                        presets: [1, 2, 3]
+                        type: 1,
+                        metadata: {
+                            customMessage: "This message was prevented by iHorizon"
+                        }
                     },
-                    actions: arrayActionsForRule
+                ];
+
+                if (logs_channel) {
+                    arrayActionsForRule.push({
+                        type: 2,
+                        metadata: {
+                            channel: logs_channel,
+                        }
+                    });
+                };
+
+                if (!mentionSpamRule) {
+
+                    await interaction.guild.autoModerationRules.create({
+                        name: 'Block mass-mention spam by iHorizon',
+                        creatorId: client.user?.id,
+                        enabled: true,
+                        eventType: 1,
+                        triggerType: 5,
+                        triggerMetadata:
+                        {
+                            mentionTotalLimit: max_mention,
+                            presets: [1, 2, 3]
+                        },
+                        actions: arrayActionsForRule
+                    });
+
+                    console.log('Règle créer !')
+
+                } else if (mentionSpamRule) {
+                    await mentionSpamRule.edit({
+                        creatorId: client.user?.id,
+                        triggerMetadata:
+                        {
+                            mentionTotalLimit: max_mention,
+                            presets: [1, 2, 3]
+                        },
+                        actions: arrayActionsForRule
+                    });
+
+                    console.log('Règle modifié !')
+                };
+
+                await db.DataBaseModel({ id: db.Set, key: `${interaction.guild.id}.GUILD.GUILD_CONFIG.mass_mention`, value: "on" });
+                await interaction.editReply({
+                    content: data.automod_block_massmention_command_on
+                        .replace('${interaction.user}', interaction.user)
+                        .replace('${logs_channel}', logs_channel || 'None')
+                        .replace('${max_mention}', max_mention)
                 });
-
-                console.log('Règle créer !')
-
-            } else if (mentionSpamRule) {
-                await mentionSpamRule.edit({
-                    creatorId: client.user?.id,
-                    triggerMetadata:
-                    {
-                        mentionTotalLimit: max_mention,
-                        presets: [1, 2, 3]
-                    },
-                    actions: arrayActionsForRule
-                });
-
-                console.log('Règle modifié !')
-            };
-
-            await db.DataBaseModel({ id: db.Set, key: `${interaction.guild.id}.GUILD.GUILD_CONFIG.mass_mention`, value: "on" });
-            await interaction.editReply({
-                content: data.automod_block_massmention_command_on
-                    .replace('${interaction.user}', interaction.user)
-                    .replace('${logs_channel}', logs_channel || 'None')
-                    .replace('${max_mention}', max_mention)
-            });
-            return;
+                return;
+            } catch {
+                interaction.editReply({ content: 'Error 404' });
+            }
         } else if (turn === "off") {
 
             await mentionSpamRule.setEnabled(false);
