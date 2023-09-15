af5e9cec22984006ee478e37443bb625e142823f
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2023-09-15 22:09:43 +0200

    Create emojis.ts

---
+++ src/Commands/utils/emojis.ts
@@ -0,0 +1,73 @@
+/*
+・ iHorizon Discord Bot (https://github.com/ihrz/ihrz)
+
+・ Licensed under the Attribution-NonCommercial-ShareAlike 2.0 Generic (CC BY-NC-SA 2.0)
+
+    ・   Under the following terms:
+
+        ・ Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+
+        ・ NonCommercial — You may not use the material for commercial purposes.
+
+        ・ ShareAlike — If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
+
+        ・ No additional restrictions — You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+
+
+・ Mainly developed by Kisakay (https://github.com/Kisakay)
+
+・ Copyright © 2020-2023 iHorizon
+*/
+
+import {
+    Client,
+    ApplicationCommandOptionType,
+    EmbedBuilder
+} from 'discord.js'
+
+import { Command } from '../../../types/command';
+
+export const command: Command = {
+    name: 'emojis',
+    description: 'Add emojis to your server easly',
+    category: 'utils',
+    options: [
+        {
+            name: 'emojis',
+            type: ApplicationCommandOptionType.String,
+            description: 'What the emoji then?',
+            required: true,
+        },
+    ],
+    run: async (client: Client, interaction: any) => {
+        let data = await client.functions.getLanguageData(interaction.guild.id);
+        let str = interaction.options.getString('emojis').split(' ')
+        let cnt: number = 0;
+        let nemj: string = '';
+        for (let emoji of str) {
+            let match = emoji.match(/:(\w+):(\d+)>/);
+            if (match) {
+                let isAnimated = emoji.startsWith('<a:');
+
+                await interaction.guild.emojis.create({
+                    attachment: `https://cdn.discordapp.com/emojis/${match[2]}.${isAnimated ? 'gif' : 'png'}`,
+                    name: match[1]
+                }).then((emoji: any) => {
+                    interaction.channel.send(`__Created new emoji__ with name **${emoji.name}**! (${emoji})`);
+                    cnt++;
+                    nemj += `<${isAnimated ? 'a:' : ':'}${emoji.name}:${emoji.id}>`
+                }).catch((err: any) => {
+                    interaction.channel.send(`__Error when create new emoji__ with name **${emoji.name}**!`);
+                });
+            }
+        }
+
+        let embed = new EmbedBuilder()
+            .setColor('#bea9de')
+            .setFooter({ text: 'iHorizon', iconURL: client.user?.displayAvatarURL() })
+            .setTimestamp()
+            .setDescription(`__Created **${cnt}** emoji(s) in \`${interaction.guild.name}\`__\n(${nemj})`)
+
+        await interaction.editReply({ embeds: [embed] });
+    },
+};
