83b8b0fc1b031743cd267e8d5cfe2f3abb2cd5f0
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2023-09-19 15:32:21 +0200

    Update gw.ts

--- src/Commands/giveaway/gw.ts
+++ src/Commands/giveaway/gw.ts
@@ -82,7 +82,7 @@ export const command: Command = {
             ],
         }
     ],
-    category: 'giveaway2',
+    category: 'giveaway',
     run: async (client: Client, interaction: any) => {
         let data = await client.functions.getLanguageData(interaction.guild.id);
         let command: any = interaction.options.getSubcommand();
