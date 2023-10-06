87cca945ddbf09c5a8dac69a37e0dc8413e232b8
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2023-10-06 17:08:12 +0200

    Update captcha.ts

--- src/core/captcha.ts
+++ src/core/captcha.ts
@@ -19,13 +19,15 @@
 ・ Copyright © 2020-2023 iHorizon
 */
 
-let { createCanvas } = require("canvas");
+import { createCanvas } from 'canvas';
+
 let FONTBASE = 200;
 let FONTSIZE = 35;
 
 let relativeFont = (width: number) => {
   let ratio = FONTSIZE / FONTBASE;
   let size = width * ratio;
+
   return `${size}px serif`;
 };
 
@@ -35,28 +37,29 @@ let randomRotation = (degrees = 15) => (arbitraryRandom(-degrees, degrees) * Mat
 let alternateCapitals = (str: string) =>
   [...str].map((char, i) => char?.[`to${i % 2 ? "Upper" : "Lower"}Case`]()).join("");
 
-// Get a random string of alphanumeric characters
-let randomText = () =>
-  alternateCapitals(
-    Math.random()
-      .toString(36)
-      .substring(2, 8)
-  );
+let randomText = () => alternateCapitals(Math.random().toString(36).substring(2, 8));
 
 let configureText = (ctx: { font: string; textBaseline: string; textAlign: string; fillText: (arg0: string, arg1: number, arg2: number) => void; }, width: number, height: number) => {
+
   ctx.font = relativeFont(width);
   ctx.textBaseline = "middle";
   ctx.textAlign = "center";
+
   let text = randomText();
+
   ctx.fillText(text, width / 2, height / 2);
+
   return text;
 };
 
 let generate = (width: number, height: number) => {
   let canvas = createCanvas(width, height);
   let ctx = canvas.getContext("2d");
+
   ctx.rotate(randomRotation());
+
   let text = configureText(ctx, width, height);
+
   return {
     image: canvas.toDataURL(),
     text: text
