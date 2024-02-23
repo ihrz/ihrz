790b2ff432dd43aca5a9ca1adb7cbccd8ad7d457
Anaïs <53227802+Kisakay@users.noreply.github.com>
2024-02-23 11:07:12 +0100

    Update temporary_voice_privacy_button.ts

--- src/Interaction/Components/Buttons/temporary_voice_privacy_button.ts
+++ src/Interaction/Components/Buttons/temporary_voice_privacy_button.ts
@@ -19,227 +19,87 @@
 ・ Copyright © 2020-2024 iHorizon
 */
 
-import { ActionRowBuilder, ButtonInteraction, CacheType, ComponentType, EmbedBuilder, Guild, GuildMember, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from 'discord.js';
+import { ActionRowBuilder, ButtonInteraction, CacheType, ComponentType, EmbedBuilder, GuildMember, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from 'discord.js';
 import { LanguageData } from '../../../../types/languageData';
 
-export default async function (interaction: ButtonInteraction<CacheType>) {
-
+export default async function handleButtonInteraction(interaction: ButtonInteraction<CacheType>) {
     let result = await interaction.client.db.get(`${interaction.guildId}.VOICE_INTERFACE.interface`);
     let table = interaction.client.db.table('TEMP');
-
     let lang = await interaction.client.functions.getLanguageData(interaction.guildId) as LanguageData;
     let member = interaction.member as GuildMember;
-
-    let targetedChannel = (interaction.member as GuildMember).voice.channel;
+    let targetedChannel = member.voice.channel;
     let getChannelOwner = await table.get(`CUSTOM_VOICE.${interaction.guild?.id}.${interaction.user.id}`);
 
-    if (result.channelId !== interaction.channelId
-        || getChannelOwner !== targetedChannel?.id) return interaction.deferUpdate();
-
-    if (!member.voice.channel) {
-        await interaction.deferUpdate()
-        return;
-    } else {
-
-        let comp = new StringSelectMenuBuilder()
-            .setCustomId('tempmorary_voice_privacy_menu')
-            .setPlaceholder('Select a Privacy Option')
-            .addOptions(
-                new StringSelectMenuOptionBuilder()
-                    .setLabel('Lock channel')
-                    .setDescription('Only trusted users will be able to join your voice channel')
-                    .setEmoji(interaction.client.iHorizon_Emojis.vc.CloseAccess)
-                    .setValue('temporary_channel_lock_channel_menu'),
-                new StringSelectMenuOptionBuilder()
-                    .setLabel('Unlock channel')
-                    .setDescription('Everyone will be able to join your voice channel')
-                    .setEmoji(interaction.client.iHorizon_Emojis.vc.OpenAcces)
-                    .setValue('temporary_channel_unlock_channel_menu'),
-                new StringSelectMenuOptionBuilder()
-                    .setLabel('Invisible')
-                    .setEmoji(interaction.client.iHorizon_Emojis.vc.Unseeable)
-                    .setDescription('Only trusted users will be able to view your voice channel')
-                    .setValue('temporary_channel_invisible_channel_menu'),
-                new StringSelectMenuOptionBuilder()
-                    .setLabel('Visible')
-                    .setEmoji(interaction.client.iHorizon_Emojis.vc.Seeable)
-                    .setDescription('Everyone will be able to view your voice channel')
-                    .setValue('temporary_channel_visible_channel_menu'),
-                new StringSelectMenuOptionBuilder()
-                    .setLabel('Close chat')
-                    .setEmoji(interaction.client.iHorizon_Emojis.vc.CloseChat)
-                    .setDescription('Only trusted users will be able to text in your chat')
-                    .setValue('temporary_channel_closechat_channel_menu'),
-                new StringSelectMenuOptionBuilder()
-                    .setLabel('Open chat')
-                    .setDescription('Everyone will be able to text in your chat')
-                    .setEmoji(interaction.client.iHorizon_Emojis.vc.OpenChat)
-                    .setValue('temporary_channel_openchat_channel_menu'),
-            );
-
-        let response = await interaction.reply({
-            ephemeral: true,
-            components: [
-                new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(comp)
-            ]
-        });
-
-        let collector = interaction.channel?.createMessageComponentCollector({
-            componentType: ComponentType.StringSelect,
-            filter: (u) => u.user.id === interaction.user.id,
-            time: 200_000
-        });
-
-        collector?.on('collect', async i => {
-
-            let value = i.values[0];
-            let action = comp.options.filter(i => i.data.value === value)[0].data.label;
-
-            let embed = new EmbedBuilder()
-                .setDescription(`## Modifications about your temporary voice channel`)
-                .setColor(2829617)
-                .setImage(`https://ihorizon.me/assets/img/banner/ihrz_${await i.client.db.get(`${interaction.guildId}.GUILD.LANG.lang`) || 'en-US'}.png`)
-                .setFooter(
-                    {
-                        text: "iHorizon",
-                        iconURL: 'attachment://icon.png'
-                    }
-                );
-
-            switch (value) {
-                // Lock channel
-                case 'temporary_channel_lock_channel_menu':
-
-                    embed.setFields(
-                        {
-                            name: `${action}`,
-                            value: `${i.client.iHorizon_Emojis.vc.CloseAccess} **Yes**`,
-                            inline: true
-                        },
-                    );
-
-                    targetedChannel?.permissionOverwrites.edit(i.guild?.roles.everyone.id as string,
-                        {
-                            Connect: false,
-                            Stream: false,
-                            Speak: false,
-                        },
-                    );
-
-                    break;
-                // Unlock channel
-                case 'temporary_channel_unlock_channel_menu':
-
-                    embed.setFields(
-                        {
-                            name: `${action}`,
-                            value: `${i.client.iHorizon_Emojis.vc.OpenAcces} **Yes**`,
-                            inline: true
-                        },
-                    );
-
-                    targetedChannel?.permissionOverwrites.edit(i.guild?.roles.everyone.id as string,
-                        {
-                            Connect: true,
-                            Stream: true,
-                            Speak: true,
-                        },
-                    );
-
-                    break;
-                // Invisible
-                case 'temporary_channel_invisible_channel_menu':
-
-                    embed.setFields(
-                        {
-                            name: `${action}`,
-                            value: `${i.client.iHorizon_Emojis.vc.Unseeable} **Yes**`,
-                            inline: true
-                        },
-                    );
-
-                    targetedChannel?.permissionOverwrites.edit(i.guild?.roles.everyone.id as string,
-                        {
-                            ViewChannel: false
-                        },
-                    );
-
-                    break;
-                // Visible
-                case 'temporary_channel_visible_channel_menu':
-
-                    embed.setFields(
-                        {
-                            name: `${action}`,
-                            value: `${i.client.iHorizon_Emojis.vc.Seeable} **Yes**`,
-                            inline: true
-                        },
-                    );
-
-                    targetedChannel?.permissionOverwrites.edit(i.guild?.roles.everyone.id as string,
-                        {
-                            ViewChannel: true
-                        },
-                    );
-
-                    break;
-                // Close chat
-                case 'temporary_channel_closechat_channel_menu':
-
-                    embed.setFields(
-                        {
-                            name: `${action}`,
-                            value: `${i.client.iHorizon_Emojis.vc.CloseChat} **Yes**`,
-                            inline: true
-                        },
-                    );
-
-                    targetedChannel?.permissionOverwrites.edit(i.guild?.roles.everyone.id as string,
-                        {
-                            SendMessages: false
-                        },
-                    );
-
-                    break;
-                // Open chat
-                case 'temporary_channel_openchat_channel_menu':
-
-                    embed.setFields(
-                        {
-                            name: `${action}`,
-                            value: `${i.client.iHorizon_Emojis.vc.OpenChat} **Yes**`,
-                            inline: true
-                        },
-                    );
-
-                    targetedChannel?.permissionOverwrites.edit(i.guild?.roles.everyone.id as string,
-                        {
-                            SendMessages: true,
-                            AddReactions: true,
-                            UseApplicationCommands: true
-                        },
-                    );
-
-                    break;
-            };
-
-            await i.reply({
-                embeds: [
-                    embed
-                ],
-                files: [
-                    {
-                        attachment: await interaction.client.functions.image64(interaction.client.user?.displayAvatarURL()),
-                        name: 'icon.png'
-                    }
-                ],
-                ephemeral: true
-            });
-        });
-
-        collector?.on('end', i => {
-            response.delete();
-        })
-
+    if (result.channelId !== interaction.channelId || getChannelOwner !== targetedChannel?.id) {
+        return interaction.deferUpdate();
     }
+
+    if (!targetedChannel) {
+        await interaction.deferUpdate();
+        return;
+    }
+
+    let comp = new StringSelectMenuBuilder()
+        .setCustomId('tempmorary_voice_privacy_menu')
+        .setPlaceholder('Select a Privacy Option')
+        .addOptions(
+            { label: 'Lock channel', description: 'Only trusted users will be able to join your voice channel', emoji: interaction.client.iHorizon_Emojis.vc.CloseAccess, value: 'temporary_channel_lock_channel_menu' },
+            { label: 'Unlock channel', description: 'Everyone will be able to join your voice channel', emoji: interaction.client.iHorizon_Emojis.vc.OpenAcces, value: 'temporary_channel_unlock_channel_menu' },
+            { label: 'Invisible', description: 'Only trusted users will be able to view your voice channel', emoji: interaction.client.iHorizon_Emojis.vc.Unseeable, value: 'temporary_channel_invisible_channel_menu' },
+            { label: 'Visible', description: 'Everyone will be able to view your voice channel', emoji: interaction.client.iHorizon_Emojis.vc.Seeable, value: 'temporary_channel_visible_channel_menu' },
+            { label: 'Close chat', description: 'Only trusted users will be able to text in your chat', emoji: interaction.client.iHorizon_Emojis.vc.CloseChat, value: 'temporary_channel_closechat_channel_menu' },
+            { label: 'Open chat', description: 'Everyone will be able to text in your chat', emoji: interaction.client.iHorizon_Emojis.vc.OpenChat, value: 'temporary_channel_openchat_channel_menu' }
+        );
+
+    let response = await interaction.reply({
+        ephemeral: true,
+        components: [new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(comp)]
+    });
+
+    let collector = interaction.channel?.createMessageComponentCollector({
+        componentType: ComponentType.StringSelect,
+        filter: (u) => u.user.id === interaction.user.id,
+        time: 200_000
+    });
+
+    collector?.on('collect', async i => {
+        let value = i.values[0];
+        let action = comp.options.find(option => option.data.value === value)?.data.label;
+
+        if (!action) return;
+
+        let embed = new EmbedBuilder()
+            .setDescription(`## Modifications about your temporary voice channel`)
+            .setColor(2829617)
+            .setImage(`https://ihorizon.me/assets/img/banner/ihrz_${await i.client.db.get(`${interaction.guildId}.GUILD.LANG.lang`) || 'en-US'}.png`)
+            .setFooter({ text: "iHorizon", iconURL: 'attachment://icon.png' });
+
+        switch (value) {
+            case 'temporary_channel_lock_channel_menu':
+            case 'temporary_channel_unlock_channel_menu':
+                embed.setFields({ name: `${action}`, value: `${value === 'temporary_channel_lock_channel_menu' ? interaction.client.iHorizon_Emojis.vc.CloseAccess : interaction.client.iHorizon_Emojis.vc.OpenAcces} **Yes**`, inline: true });
+                targetedChannel?.permissionOverwrites.edit(i.guild?.roles.everyone.id as string, { Connect: value === 'temporary_channel_lock_channel_menu' ? false : true, Stream: value === 'temporary_channel_lock_channel_menu' ? false : true, Speak: value === 'temporary_channel_lock_channel_menu' ? false : true });
+                break;
+            case 'temporary_channel_invisible_channel_menu':
+            case 'temporary_channel_visible_channel_menu':
+                embed.setFields({ name: `${action}`, value: `${value === 'temporary_channel_invisible_channel_menu' ? interaction.client.iHorizon_Emojis.vc.Unseeable : interaction.client.iHorizon_Emojis.vc.Seeable} **Yes**`, inline: true });
+                targetedChannel?.permissionOverwrites.edit(i.guild?.roles.everyone.id as string, { ViewChannel: value === 'temporary_channel_invisible_channel_menu' ? false : true });
+                break;
+            case 'temporary_channel_closechat_channel_menu':
+            case 'temporary_channel_openchat_channel_menu':
+                embed.setFields({ name: `${action}`, value: `${value === 'temporary_channel_closechat_channel_menu' ? interaction.client.iHorizon_Emojis.vc.CloseChat : interaction.client.iHorizon_Emojis.vc.OpenChat} **Yes**`, inline: true });
+                targetedChannel?.permissionOverwrites.edit(i.guild?.roles.everyone.id as string, { SendMessages: value === 'temporary_channel_closechat_channel_menu' ? false : true, AddReactions: value === 'temporary_channel_closechat_channel_menu' ? false : true, UseApplicationCommands: value === 'temporary_channel_closechat_channel_menu' ? false : true });
+                break;
+        }
+
+        await i.reply({
+            embeds: [embed],
+            files: [{ attachment: await interaction.client.functions.image64(interaction.client.user?.displayAvatarURL()), name: 'icon.png' }],
+            ephemeral: true
+        });
+    });
+
+    collector?.on('end', () => {
+        response.delete();
+    });
 };
