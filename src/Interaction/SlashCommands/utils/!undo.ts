92a2484339811fabfbc8198bae75dcf926b9c492
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2024-03-04 17:28:12 +0100

    Hey hey hey EAAA EOOOOO AA

---
+++ src/Interaction/SlashCommands/utils/!undo.ts
@@ -0,0 +1,80 @@
+/*
+・ iHorizon Discord Bot (https://github.com/ihrz/ihrz)
+
+・ Licensed under the Attribution-NonCommercial-ShareAlike 2.0 Generic (CC BY-NC-SA 2.0)
+
+    ・   Under the following terms:
+
+        ・ Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+
+        ・ NonCommercial — You may not use the material for commercial purposes.
+
+        ・ ShareAlike — If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
+
+        ・ No additional restrictions — You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+
+
+・ Mainly developed by Kisakay (https://github.com/Kisakay)
+
+・ Copyright © 2020-2024 iHorizon
+*/
+
+import {
+    ChatInputCommandInteraction,
+    Client,
+    EmbedBuilder,
+    PermissionsBitField
+} from 'discord.js';
+import { LanguageData } from '../../../../types/languageData';
+import wait from '../../../core/functions/wait.js';
+
+export default {
+    run: async (client: Client, interaction: ChatInputCommandInteraction, data: LanguageData) => {
+
+        if (!interaction.memberPermissions?.has(PermissionsBitField.Flags.Administrator)) {
+            await interaction.editReply({ content: data.prevnames_not_admin });
+            return;
+        };
+
+        let unbanned_members = await client.db.get(`${interaction.guildId}.UTILS.unban_members`);
+        let banned_members: string[] = [];
+        let cannot_ban = 0;
+
+        if (unbanned_members) {
+            await Promise.all(unbanned_members.map(async (index: string) => {
+                try {
+                    await interaction.guild?.bans.create(index);
+                    banned_members.push(index);
+                } catch (e) {
+                    cannot_ban++;
+                }
+                await wait(800);
+            }));
+        }
+
+        await client.db.set(`${interaction.guildId}.UTILS.unban_members`, []);
+
+        await interaction.editReply({
+            embeds: [
+                new EmbedBuilder()
+                    .setColor(2829617)
+                    .setDescription(
+                        data.action_unban_undo_embed_desc
+                            .replace("${client.iHorizon_Emojis.icon.Yes_Logo}", client.iHorizon_Emojis.icon.Yes_Logo)
+                            .replace('${banned_members.length}', banned_members.length.toString())
+                            .replace("${client.iHorizon_Emojis.icon.No_Logo}", client.iHorizon_Emojis.icon.No_Logo)
+                            .replace('${cannot_ban}', cannot_ban.toString())
+                    )
+                    .setFooter({ text: 'iHorizon', iconURL: "attachment://icon.png" })
+            ],
+            files: [
+                {
+                    attachment: await interaction.client.functions.image64(interaction.client.user?.displayAvatarURL()),
+                    name: 'icon.png'
+                }
+            ]
+        });
+
+        return;
+    },
+};
---
+++ src/Interaction/SlashCommands/utils/action.ts
@@ -0,0 +1,87 @@
+/*
+・ iHorizon Discord Bot (https://github.com/ihrz/ihrz)
+
+・ Licensed under the Attribution-NonCommercial-ShareAlike 2.0 Generic (CC BY-NC-SA 2.0)
+
+    ・   Under the following terms:
+
+        ・ Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+
+        ・ NonCommercial — You may not use the material for commercial purposes.
+
+        ・ ShareAlike — If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
+
+        ・ No additional restrictions — You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+
+
+・ Mainly developed by Kisakay (https://github.com/Kisakay)
+
+・ Copyright © 2020-2024 iHorizon
+*/
+
+import {
+    ApplicationCommandOptionType,
+    ApplicationCommandType,
+    ChatInputCommandInteraction,
+    Client,
+} from 'discord.js'
+
+import { Command } from '../../../../types/command.js';
+import { LanguageData } from '../../../../types/languageData.js';
+import wait from '../../../core/functions/wait.js';
+
+export const command: Command = {
+    name: 'action',
+
+    description: "Subcommand group for action (unban all, massrole...)",
+    description_localizations: {
+        "fr": "Groupe de sous-commandes pour l'action (débannir tout, massrole...)"
+    },
+
+    options: [
+        {
+            name: "unban",
+
+            description: "Mass action about unban",
+            description_localizations: {
+                "fr": "Action de masse pour débannir"
+            },
+
+            type: ApplicationCommandOptionType.SubcommandGroup,
+
+            options: [
+                {
+                    name: "all",
+
+                    description: "Unban all member of the guild",
+                    description_localizations: {
+                        "fr": "Bannir tout le monde sur un serveur"
+                    },
+
+                    type: ApplicationCommandOptionType.Subcommand
+                },
+                {
+                    name: "undo",
+
+                    description: "Undo the unban all of the guild",
+                    description_localizations: {
+                        "fr": "Annuler le dé-bannissement de tout les serveurs"
+                    },
+
+                    type: ApplicationCommandOptionType.Subcommand
+                }
+            ]
+        }
+    ],
+
+    category: 'utils',
+    thinking: true,
+    type: ApplicationCommandType.ChatInput,
+    run: async (client: Client, interaction: ChatInputCommandInteraction) => {
+        let data = await client.functions.getLanguageData(interaction.guild?.id);
+        let command = interaction.options.getSubcommand();
+
+        const commandModule = await import(`./!${command}.js`);
+        await commandModule.default.run(client, interaction, data);
+    },
+};
