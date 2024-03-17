8901630f803e03933559371b18427774d3e896df
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2024-03-17 14:21:35 +0100

    Update guildCreate.ts

--- src/Events/guildCreate.ts
+++ src/Events/guildCreate.ts
@@ -145,7 +145,10 @@ export default async (client: Client, guild: Guild) => {
             }
         };
 
-        let embed = new EmbedBuilder().setColor("#ff0505").setTimestamp(guild.joinedTimestamp).setAuthor({name: `**I won a guild!**`})
+        let embed = new EmbedBuilder()
+            .setColor("#1DAB6F")
+            .setTimestamp(guild.joinedTimestamp)
+            .setDescription("## New Guild have added me !")
             .addFields(
                 { name: "・Server", value: `\`\`\`py\n${guild.name} (ID: ${guild.id})\`\`\``, inline: true },
                 { name: "・Server Region", value: `\`\`\`py\n${guild.preferredLocale}\`\`\``, inline: true },
