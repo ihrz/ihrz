4d38c804522d48e0e2f8a9df7f8b439d1c1b9042
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2024-05-09 15:40:31 +0000

    Update !join-dm.ts

--- src/Interaction/SlashCommands/guildconfig/!join-dm.ts
+++ src/Interaction/SlashCommands/guildconfig/!join-dm.ts
@@ -135,7 +135,7 @@ export default {
 
                 try {
                     let logEmbed = new EmbedBuilder()
-                        .setColor("#bf0bb9")
+                        .setColor(await client.db.get(`${interaction.guild?.id}.GUILD.GUILD_CONFIG.embed_color.ihrz-logs`) || "#bf0bb9")
                         .setTitle(data.setjoindm_logs_embed_title_on_disable)
                         .setDescription(data.setjoindm_logs_embed_description_on_disable
                             .replace(/\${interaction\.user\.id}/g, interaction.user.id)
