fe8af687ff9bd967e1cbe3745ebf67db79b38866
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2024-05-17 21:14:53 +0000

    Fix stupid misstake with /ranks disable

--- src/Interaction/SlashCommands/ranks/!disable.ts
+++ src/Interaction/SlashCommands/ranks/!disable.ts
@@ -58,7 +58,7 @@ export default {
 
             await client.db.set(`${interaction.guildId}.GUILD.XP_LEVELING.disable`, false);
 
-            await interaction.reply({ content: data.disablexp_command_work_disable_entierly });
+            await interaction.reply({ content: data.disablexp_command_work_disable });
             return;
         } else if (types == "disable") {
             try {
@@ -77,8 +77,8 @@ export default {
             };
 
             await client.db.set(`${interaction.guildId}.GUILD.XP_LEVELING.disable`, 'disable');
-
-            await interaction.reply({ content: data.disablexp_command_work_disable });
+            
+            await interaction.reply({ content: data.disablexp_command_work_disable_entierly });
             return;
         } else if (types == "on") {
             try {
