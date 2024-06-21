5b67cf58a560dbf37c2f8db25a3dc8b22f0ea48a
Kisakay <53227802+Kisakay@users.noreply.github.com>
2024-06-21 10:05:07 +0200

    Update getLanguageData.ts

--- src/core/functions/getLanguageData.ts
+++ src/core/functions/getLanguageData.ts
@@ -52,6 +52,7 @@ export default async function getLanguageData(arg: string): Promise<LanguageData
         dat = yaml.load(fs.readFileSync(path.join(__dirname, '..', '..', '..', '..', 'src', 'lang', lang + '.yml'), 'utf8')
             .replaceAll('iHorizon ', cached_client.user?.username!)
             .replaceAll(' iHorizon', cached_client.user?.username!)
+            .replaceAll('iHorizon.', cached_client.user?.username!)
         ) as LanguageData;
         LangsData[lang] = dat;
     };
