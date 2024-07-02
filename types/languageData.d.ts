13c05abefc581778273563014f349f67a0917081
Kisakay <53227802+Kisakay@users.noreply.github.com>
2024-07-02 21:58:29 +0200

    .

--- types/languageData.d.ts
+++ types/languageData.d.ts
@@ -589,6 +589,7 @@ export interface LanguageData {
   guildconfig_setbot_footername_footer_too_long_msg: string;
   guildconfig_setbot_footername_footer_too_long_msg: string;
   guildconfig_setbot_footername_is_good: string;
+  guildconfig_setbot_footeravatar_is_good: string;
   guildconfig_setbot_prefix_prefix_now_mention: string;
   guildconfig_setbot_prefix_prefix_specify_prefix: string;
   guildconfig_setbot_prefix_prefix_too_long: string;
