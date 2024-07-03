e17b22769c10b750bea63f593b3bbc8fd3ed8038
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2024-07-03 09:30:05 +0200

    Create echo.ts

---
+++ src/core/bash/commands/echo.ts
@@ -0,0 +1,32 @@
+/*
+・ iHorizon Discord Bot (https://github.com/ihrz/ihrz)
+
+・ Licensed under the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+
+    ・   Under the following terms:
+
+        ・ Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+
+        ・ NonCommercial — You may not use the material for commercial purposes.
+
+        ・ ShareAlike — If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
+
+        ・ No additional restrictions — You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+
+
+・ Mainly developed by Kisakay (https://github.com/Kisakay)
+
+・ Copyright © 2020-2024 iHorizon
+*/
+
+import { BashCommands } from "../../../../types/bashCommands";
+import logger from "../../logger.js";
+import { Client } from "pwss";
+
+export const command: BashCommands = {
+    command_name: "echo",
+    command_description: "Echoing back to the console",
+    run: function (client: Client, args: string) {
+        logger.legacy(args)
+    }
+};
