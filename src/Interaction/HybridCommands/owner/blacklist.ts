3991473fab5e7a37aa9c952029f70acaf4b45b9a
Anaïs <53227802+Kisakay@users.noreply.github.com>
2024-07-15 16:58:00 +0200

    Update blacklist.ts

--- src/Interaction/HybridCommands/owner/blacklist.ts
+++ src/Interaction/HybridCommands/owner/blacklist.ts
@@ -81,7 +81,7 @@ export const command: Command = {
         let tableBlacklist = client.db.table('BLACKLIST');
 
         if (await tableOwner.get(`${interaction.member.user.id}.owner`) !== true) {
-            await client.args.interactionSend(interaction,{ content: data.blacklist_not_owner });
+            await client.args.interactionSend(interaction, { content: data.blacklist_not_owner });
             return;
         };
 
@@ -100,7 +100,7 @@ export const command: Command = {
 
         if (!member && !user) {
             if (!blacklistedUsers.length) {
-                await client.args.interactionSend(interaction,{
+                await client.args.interactionSend(interaction, {
                     content: data.blacklist_no_one_blacklist
                         .replace("${client.iHorizon_Emojis.icon.No_Logo}", client.iHorizon_Emojis.icon.No_Logo),
                     ephemeral: true
@@ -150,7 +150,7 @@ export const command: Command = {
                     .setStyle(ButtonStyle.Secondary),
             );
 
-            let messageEmbed = await client.args.interactionSend(interaction,{
+            let messageEmbed = await client.args.interactionSend(interaction, {
                 embeds: [createEmbed()],
                 components: [row],
                 files: [await client.args.bot.footerAttachmentBuilder(interaction)]
@@ -183,16 +183,25 @@ export const command: Command = {
             });
         };
 
+
+        let guilds = client.guilds.cache.map(guild => guild.id);
+
+        for (let guildId of guilds) {
+            let guild = client.guilds.cache.find(guild => guild.id === guildId);
+
+            await (guild?.members.cache.get(String(user?.id! || member?.id)))?.ban({ reason: reason || 'blacklisted!' });
+        };
+
         if (member) {
             if (member.user.id === client.user.id) {
-                await client.args.interactionSend(interaction,{ content: data.blacklist_bot_lol });
+                await client.args.interactionSend(interaction, { content: data.blacklist_bot_lol });
                 return;
             };
 
             let fetched = await tableBlacklist.get(`${member.user.id}`);
 
             if (fetched) {
-                await client.args.interactionSend(interaction,{
+                await client.args.interactionSend(interaction, {
                     content: data.blacklist_already_blacklisted
                         .replace(/\${member\.user\.username}/g, member.user.globalName || member.user.username)
                 });
@@ -207,13 +216,13 @@ export const command: Command = {
             });
 
             member.ban({ reason: 'blacklisted !' }).then(async () => {
-                await client.args.interactionSend(interaction,{
+                await client.args.interactionSend(interaction, {
                     content: data.blacklist_command_work
                         .replace(/\${member\.user\.username}/g, String(member?.user.globalName || member?.user.username))
                 });
                 return;
             }).catch(async () => {
-                await client.args.interactionSend(interaction,{
+                await client.args.interactionSend(interaction, {
                     content: data.blacklist_blacklisted_but_can_ban_him
                         .replace("${client.iHorizon_Emojis.icon.No_Logo}", client.iHorizon_Emojis.icon.No_Logo)
                 });
@@ -223,14 +232,14 @@ export const command: Command = {
         } else if (user) {
 
             if (user.id === client.user.id) {
-                await client.args.interactionSend(interaction,{ content: data.blacklist_bot_lol });
+                await client.args.interactionSend(interaction, { content: data.blacklist_bot_lol });
                 return;
             };
 
             let fetched = await tableBlacklist.get(`${user.id}`);
 
             if (fetched) {
-                await client.args.interactionSend(interaction,{
+                await client.args.interactionSend(interaction, {
                     content: data.blacklist_already_blacklisted
                         .replace(/\${member\.user\.username}/g, user.globalName || user.username)
                 });
@@ -244,18 +253,10 @@ export const command: Command = {
                 createdAt: new Date().getTime()
             });
 
-            await client.args.interactionSend(interaction,{
+            await client.args.interactionSend(interaction, {
                 content: data.blacklist_command_work
                     .replace(/\${member\.user\.username}/g, user.globalName || user.username)
             });
-
-            let guilds = client.guilds.cache.map(guild => guild.id);
-
-            for (let guildId of guilds) {
-                let guild = client.guilds.cache.find(guild => guild.id === guildId);
-
-                guild?.members.cache.get(user.id)?.ban({ reason: reason || 'blacklisted!' });
-            };
 
             return;
         }
