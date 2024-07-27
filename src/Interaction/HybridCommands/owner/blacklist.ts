7ff6e42e2f981cf1465c6d07ccad36069dfc4c70
Kisakay <53227802+Kisakay@users.noreply.github.com>
2024-07-27 18:01:47 +0200

    Update blacklist.ts

--- src/Interaction/HybridCommands/owner/blacklist.ts
+++ src/Interaction/HybridCommands/owner/blacklist.ts
@@ -94,7 +94,7 @@ export const command: Command = {
             var _ = await client.method.checkCommandArgs(interaction, command, args!, lang); if (!_) return;
             var member = client.method.member(interaction, args!, 0) as GuildMember | null;
             var user = await client.method.user(interaction, args!, 0);
-            var reason = client.method.longString(args!, 0);
+            var reason = client.method.longString(args!, 1);
         };
 
         if (!member && !user) {
