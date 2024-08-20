c7a7316e95ba31e242054adb044e0b15781744d9
Anaïs <53227802+Kisakay@users.noreply.github.com>
2024-08-20 18:04:23 +0200

    .

--- types/configDatad.d.ts
+++ types/configDatad.d.ts
@@ -84,7 +84,7 @@ export interface ConfigData {
     };
 
     database?: {
-        method: 'MONGO_DB' | 'JSON' | 'MYSQL' | 'SQLITE' | 'CACHED_SQL' | 'POSTGRES' | 'PNG'
+        method: 'MONGO_DB' | 'JSON' | 'MYSQL' | 'SQLITE' | 'CACHED_SQL' | 'POSTGRES' | 'PNG' | 'JSON2'
 
         mongoDb?: string;
 
