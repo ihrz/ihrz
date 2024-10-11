16595242c35e8f48d994c648fba848c818edc6fd
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2024-10-11 15:16:24 +0200

    /

--- src/Interaction/SlashCommands/newfeatures/punishpub.ts
+++ src/Interaction/SlashCommands/newfeatures/punishpub.ts
@@ -104,7 +104,7 @@ export const command: Command = {
     category: 'newfeatures',
     type: ApplicationCommandType.ChatInput,
     run: async (client: Client, interaction: ChatInputCommandInteraction<"cached">, lang: LanguageData, command: SubCommandArgumentValue) => {        
-        let permCheck = await client.method.permission.checkCommandPermission(interaction, command.command!);
+        let permCheck = await client.method.permission.checkCommandPermission(interaction, command as unknown as Command);
         if (!permCheck.allowed) return client.method.permission.sendErrorMessage(interaction, lang, permCheck.neededPerm || 0);
 
         // Guard's Typing
