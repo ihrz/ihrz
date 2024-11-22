855498774df9fef5c25df1a81e4c02706dda5307
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2024-11-22 17:30:11 +0100

    Update DatabaseModel.ts

--- src/core/functions/DatabaseModel.ts
+++ src/core/functions/DatabaseModel.ts
@@ -27,7 +27,7 @@ let db;
 
 if (!db) {
     db = new Promise<QuickDB>((resolve, reject) => {
-        logger.log(`${config.console.emojis.HOST} >> Connected to the database (JSON) !`.green);
+        logger.log(`${config.console.emojis.HOST} >> Connected to the database (SQLITE) !`.green);
         resolve(new QuickDB({ filePath: `${process.cwd()}/src/files/db.sqlite` }));
     });
 }
