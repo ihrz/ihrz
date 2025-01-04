401580c8b4d2f1ac1a6922e95281f65d782d76b9
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-01-04 23:27:00 +0100

    .

--- dist/src/core/modules/emojisManager.js
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs';
-import toml from 'toml';
-function emojis(client) {
-    let emojis = toml.parse(fs.readFileSync(process.cwd() + "/src/files/emojis.toml", 'utf-8'));
-    client.iHorizon_Emojis = emojis;
-}
-;
-export default emojis;
