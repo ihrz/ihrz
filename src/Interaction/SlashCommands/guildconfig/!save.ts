532fcd9371069de75837bd097a10fee265974621
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-02-08 20:31:28 +0100

    Update !save.ts

--- src/Interaction/SlashCommands/guildconfig/!save.ts
+++ src/Interaction/SlashCommands/guildconfig/!save.ts
@@ -39,30 +39,20 @@ export const subCommand: SubCommand = {
         // Guard's Typing
         if (!interaction.member || !client.user || !interaction.user || !interaction.guild || !interaction.channel) return;
 
-        if (env === 'production' || env === "dev") {
-            await interaction.editReply({ content: lang.guildconfig_config_save_check_dm });
-            let link = apiUrlParser.HorizonGateway(apiUrlParser.GatewayMethod.ServerBackup);
+        const dbGuild = await client.db.get(`${interaction.guildId}`);
 
-            await interaction.user.send({ content: `${lang.guildconfig_config_save_user_msg_2}${link}/${interaction.guildId}/${encrypt(client.config.api.apiToken, Date.now().toString())}` })
-                .catch(() => { })
-                .then(() => { });
-        } else {
+        let buffer = Buffer.from(encrypt(client.config.api.apiToken, JSON.stringify(dbGuild)), 'utf-8');
+        let attachment = new AttachmentBuilder(buffer, { name: interaction.guildId + '.json' })
 
-            const dbGuild = await client.db.get(`${interaction.guildId}`);
+        await interaction.editReply({ content: lang.guildconfig_config_save_check_dm });
 
-            let buffer = Buffer.from(encrypt(client.config.api.apiToken, JSON.stringify(dbGuild)), 'utf-8');
-            let attachment = new AttachmentBuilder(buffer, { name: interaction.guildId + '.json' })
-
-            await interaction.editReply({ content: lang.guildconfig_config_save_check_dm });
-
-            await interaction.user.send({
-                content: lang.guildconfig_config_save_user_msg
-                    .replace("${interaction.guild.name}", interaction.guild.name),
-                files: [attachment]
-            })
-                .catch(() => { })
-                .then(() => { });
-        }
+        await interaction.user.send({
+            content: lang.guildconfig_config_save_user_msg
+                .replace("${interaction.guild.name}", interaction.guild.name),
+            files: [attachment]
+        })
+            .catch(() => { })
+            .then(() => { });
         return
     },
 };
