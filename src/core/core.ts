f6fd11b4c19645b0e486bd21222feb9d60b8609d
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-03-08 15:03:36 +0100

    Fix

--- src/core/core.ts
+++ src/core/core.ts
@@ -55,7 +55,6 @@ import { AutoRenew } from './modules/autorenewManager.js';
 import config from '../files/config.js';
 import { Client_Functions } from '../../types/client_functions.js';
 import { AnotherCommand } from '../../types/anotherCommand.js';
-import { load_cache } from '../load_cache.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -133,7 +132,6 @@ export async function main(client: Client) {
 	emojis(client);
 	if (process.env.CACHE === "true") {
 		logger.log(`[${config.console.emojis.LOAD}] Cache is enabled, initializing cache storage...`.bgBlack.gray.boldText);
-		await load_cache(client);
 	} else {
 		let handlerPath = path.join(__dirname, '..', 'core', 'handlers');
 		let handlerFiles = (await readdir(handlerPath)).filter(file => file.endsWith('.js'));
