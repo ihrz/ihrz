aaf9361ea125bd3223d300e0ee48d1b1c4dbb52d
Anaïs Saraiva <kisakay@mailfence.com>
2025-06-27 23:47:18 +0200

    Update blacklist.ts

--- src/Interaction/HybridCommands/owner/blacklist.ts
+++ src/Interaction/HybridCommands/owner/blacklist.ts
@@ -172,7 +172,7 @@ export const command: Command = {
 				filter: async (i) => {
 					await i.deferUpdate(); return interaction.member?.user.id === i.user.id;
 				},
-				time: 60000
+				time: 60_000 * 16 // 16 minutes
 			});
 
 			collector.on('collect', (interaction: { customId: string; }) => {
