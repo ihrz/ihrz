c0307d0ec29c0dd25a9a127970573e21753823fa
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-07-22 22:10:36 +0200

    Create shardinfo.ts

---
+++ src/Interaction/MessageCommands/utils/shardinfo.ts
@@ -0,0 +1,58 @@
+/*
+・ iHorizon Discord Bot (https://gitlab.com/ihrz/ihrz)
+
+・ Licensed under the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+
+	・   Under the following terms:
+
+		・ Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+
+		・ NonCommercial — You may not use the material for commercial purposes.
+
+		・ ShareAlike — If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
+
+		・ No additional restrictions — You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+
+
+・ Mainly developed by Kisakay (https://gitlab.com/Kisakay)
+
+・ Copyright © 2020-2025 iHorizon
+*/
+
+import {
+	BaseGuildTextChannel,
+	Client,
+	EmbedBuilder,
+	Message,
+	time,
+} from 'discord.js';
+
+import { LanguageData } from '../../../../types/languageData.js';
+import { Command } from '../../../../types/command.js';
+
+
+export const command: Command = {
+	name: 'shardinfo',
+	aliases: [],
+
+	description: '...',
+	description_localizations: {
+		"fr": "..."
+	},
+
+	thinking: false,
+	category: 'owner',
+	type: "PREFIX_IHORIZON_COMMAND",
+
+	permission: null,
+	run: async (client: Client, message: Message<true>, lang: LanguageData, options?: string[]) => {
+		if (client.owners.includes(message.author.id)) {
+			let msg = `this client is on shard: ${process.pid}
+this server is on it? ${client.inShard(message.guildId) ? "yes" : "no"}
+number of shard(s): ${client.shard?.count}
+shard ids: ${client.shard?.ids}`
+				;
+			message.reply({ content: msg })
+		} else return;
+	},
+};
