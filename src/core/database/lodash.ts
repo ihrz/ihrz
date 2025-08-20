703061ed22994c35e2c7a56ad24439afeea239b6
Anaïs Saraivas <anais@proserv.home>
2025-08-20 11:18:49 +0200

    Add the headers to the rest

--- src/core/database/lodash.ts
+++ src/core/database/lodash.ts
@@ -1,3 +1,24 @@
+/*
+・ iHorizon Discord Bot (https://gitlab.com/ihrz/ihrz)
+
+・ Licensed under the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+
+	・   Under the following terms:
+
+		・ Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+
+		・ NonCommercial — You may not use the material for commercial purposes.
+
+		・ ShareAlike — If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
+
+		・ No additional restrictions — You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+
+
+・ Mainly developed by Kisakay (https://gitlab.com/Kisakay)
+
+・ Copyright © 2020-2025 iHorizon
+*/
+
 // Native implementations of lodash functions
 function get_property(obj: any, path: string, defaultValue: any = null): any {
 	if (!obj) return defaultValue;
