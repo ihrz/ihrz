a97f53a2def517e4d3ae6bd7e444027ecb662d19
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-10-29 10:26:28 +0100

    Licence header: Update all

--- src/core/modules/infrastructureMonitoringManager.ts
+++ src/core/modules/infrastructureMonitoringManager.ts
@@ -1,7 +1,7 @@
 /*
 ・ iHorizon Discord Bot (https://gitlab.com/ihrz/ihrz)
 
-・ Licensed under the Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+・ Licensed under the Attribution-NonCommercial-ShareAlike 4.0 International (CC-BY-NC-SA-4.0)
 
 	・   Under the following terms:
 
