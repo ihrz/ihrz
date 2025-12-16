6d76f4264ad1e9fe5bc454bd74d6485d82ccb2c4
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-12-16 07:03:40 +0100

    ,

--- src/Events/protection/avoidRoleUpdate.ts
+++ src/Events/protection/avoidRoleUpdate.ts
@@ -56,8 +56,9 @@ export const event: BotEvent = {
 					shouldSanction = true;
 				};
 			}
+			const isOwner = await client.db.get(`${user?.guild.id}.OWNER.${user?.id}`)
 
-			shouldSanction && (async () => {
+			!isOwner && shouldSanction && (async () => {
 				await client.func.method.punish(data, user!);
 
 				await newRole.edit({
