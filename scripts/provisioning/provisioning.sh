26f061a02253522947554fb032e485aa3501f808
Ether <ether-dsc@proton.me>
2026-05-13 19:30:23 +0200

    Add comments

--- scripts/provisioning/provisioning.sh
+++ scripts/provisioning/provisioning.sh
@@ -336,9 +336,10 @@ fi
 	# Step 6 : Starting the bot and making it daemonized
 	# Starting the bot (here the name of the daemon will be iHorizon and the interpreter will be bun)
 	echo "Setting up pm2..."
-	pm2 start ~/.bun/bin/bun --name "iHorizon" -- run src/index.ts7
-	# 
+	pm2 start ~/.bun/bin/bun --name "iHorizon" -- run src/index.ts
+	# Save pm2 config
 	pm2 save --force
+	# Launch iHorizon at startup
 	STARTUP_CMD=$(pm2 startup systemd -u $USER --hp $HOME | grep "sudo env" | tail -1)
 	eval "$STARTUP_CMD"
 
