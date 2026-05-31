8c2a293cd9d8fd1c4446574f245ad444fe2670d7
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-05-31 19:47:28 +0200

    antiExe

---
+++ src/Interaction/MessageCommands/guildconfig/@antiexe.ts
@@ -0,0 +1,66 @@
+/*
+・ iHorizon Discord Bot (https://gitlab.com/ihrz/ihrz)
+
+・ Licensed under the Attribution-NonCommercial-ShareAlike 4.0 International (CC-BY-NC-SA-4.0)
+
+	・   Under the following terms:
+
+		・ Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+
+		・ NonCommercial — You may not use the material for commercial purposes.
+
+		・ ShareAlike — If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
+
+		・ No additional restrictions — You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+
+
+・ Mainly developed by Kisakay (https://gitlab.com/Kisakay)
+
+・ Copyright © 2020-2026 iHorizon
+*/
+
+import { Client, Message, PermissionsBitField } from "discord.js";
+
+import {
+	isDiscordEmoji,
+	isSingleEmoji
+} from "../../../core/functions/emojiChecker.js";
+import { LanguageData } from "../../../../types/languageData.js";
+import { Command } from "../../../../types/command.js";
+
+export const command: Command = {
+	name: "antiexe",
+
+	description: "Delete every message containing binary files",
+	description_localizations: {
+		fr: "Supprimer systématiquement tout les messages contenant des binaires éxécutable"
+	},
+
+	thinking: false,
+	category: "guildconfig",
+	type: "PREFIX_IHORIZON_COMMAND",
+	permission: null,
+	run: async (
+		client: Client,
+		interaction: Message<true>,
+		lang: LanguageData,
+		options?: string[]
+	) => {
+		const state = await client.db.get(
+			`${interaction.guildId}.UTILS.antiExe`
+		);
+
+		const newState = state === "on" ? "off" : "on";
+
+		await client.db.set(`${interaction.guildId}.UTILS.antiExe`, newState);
+
+		await interaction.reply({
+			content:
+				newState === "on"
+					? "**[Anti-Bin]** Enabled: 🔐"
+					: "**[Anti-Bin]** Disabled: 🔓",
+			allowedMentions: { repliedUser: false }
+		});
+		return;
+	}
+};
